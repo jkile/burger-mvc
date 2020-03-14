9a1092e3f4c9e8734a8c53859af68778da95628b
Jake Kile <jkile9@gmail.com>
2020-03-13 21:53:22 -0700

    setup models

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,19 @@
 const orm = require("../config/orm");
+
+const burger = {
+    getAll(cb) {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+        });
+    },
+    createBurger(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    updateBurger(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
+            cb(res);
+        })
+    }
+}
