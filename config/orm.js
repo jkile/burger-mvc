68473d86415d12402bf36893202834948d5e63cc
Jake Kile <jkile9@gmail.com>
2020-03-13 21:44:42 -0700

    setup orm file

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,30 @@
 const connection = ("./connection");
 
 const orm = {
-    selectAll(){
-
+    selectAll(tableInput, cb){
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
     },
 
-    insertOne(){
+    insertOne(table, cols, vals, cb){
+        let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (??)`;
 
+        connection.query(queryString, vals, (err, result) => {
+            if (err) throw err;
+            cb(results);
+        })
     },
 
-    updateOne(){
-
+    updateOne(table, objColVals,condition, cb){
+        let queryString = `UPDATE ${table} SET  WHERE ${condition}`;
+        console.log(objColVals);
+        // connection.query(queryString, (err, result) => {
+        //     if (err) throw err;
+        //     cb(results);
+        // });
     }
 }
 
